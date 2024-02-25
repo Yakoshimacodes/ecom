e52748b0192b8e3c6fcee4cbbed44c0e9b559845
annnapurna <shubham.belbase@gmail.com>
2024-02-25 01:52:48 +0000

    brand and category view

--- home/views.py
+++ home/views.py
@@ -17,3 +17,17 @@ class HomeView(BaseView):
         self.views['products'] = Product.objects.all()
 
         return render(request, 'index.html', self.views)
+
+class CategoryView(BaseView):
+    def get(self, request, slug):
+        self.views
+        cat_id = Category.objects.get(slug = slug).id
+        self.views['cat_product'] = Product.objects.filter(category_id = cat_id)
+        return render(request, 'category.html', self.views)
+
+class BrandView(BaseView):
+    def get(self, request, slug):
+        self.views
+        b_id = Brand.objects.get(slug = slug).id
+        self.views['brand_product'] = Product.objects.filter(brand_id = cat_id)
+        return render(request, 'brand.html', self.views)
